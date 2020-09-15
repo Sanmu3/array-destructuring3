4135088fabcefb9648c6443d0b377aa2c2d92c58
Sanmu3 <mujahidmuslim0@gmail.com>
2020-09-15 16:28:18 +0700

    tugas ES 6 no 4

---
+++ index.js
@@ -0,0 +1,7 @@
+const arr2 = [ 'rizal', 'prabumulih', "smknegeri1prabumulih","prabujaya","sukajadi"];
+
+const [nama, asal, ...lain] = arr2
+
+console.log(nama);
+console.log(asal);
+console.log(lain);
